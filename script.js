3f58e239bfb41e49371c40e8849c582eb14f8377
Prakritipi <prakritiacharyaa@gmail.com>
2025-03-03 17:45:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ const menu = document.querySelector('.menu');
 hamburger.addEventListener('click', function () {
     const hamIcon = this.querySelector('.hamburger-icon');
     const crossIcon = this.querySelector('.cross-icon');
-    if (hamIcon.style.display === "none") {
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    }
-    else {
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
+    
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        hamIcon.style.display = "inline-block";
+        crossIcon.style.display = "none";
+    } else {
+        menu.style.display = "block";
+        hamIcon.style.display = "none";
+        crossIcon.style.display = "inline-block";
     }
 });
+
+/* Ensure menu resets when screen size increases */
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 768) {
+        menu.style.display = "flex"; // Show menu normally on larger screens
+    } else {
+        menu.style.display = "none"; // Hide menu again if resized to small screen
+    }
+});
+
+
+
